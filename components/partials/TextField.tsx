effef2c595254aa4b94c01d03ea3cfc853eb6fa3
Dexter Bigueta <dexter@course.studio>
2023-08-15 19:58:00 -0700

    Finished tooltips

--- components/partials/TextField.tsx
+++ components/partials/TextField.tsx
@@ -2,7 +2,11 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import { Tooltip } from '@nextui-org/tooltip';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
+import useMediaQuery from '@/src/hooks/useMediaQuery';
+import { SCREEN_MD } from '@/src/constants';
 
 type TextFieldProps = {
   type?: 'number' | 'text';
@@ -22,24 +26,25 @@ const TextField = ({
   tooltipText = '',
 }: TextFieldProps) => {
   const [tooltipClicked, setTooltipClicked] = useState(false);
+  const isTablet = useMediaQuery(SCREEN_MD);
   return (
     <div className="">
       <label>
         <div className="flex items-center gap-2 mb-2">
           <p className="text-sm">{label}</p>
           {tooltip && tooltipText !== '' && (
-            <Tooltip isOpen={tooltipClicked} onOpenChange={(open) => setTooltipClicked(open)} content={tooltipText}>
-              <figure
-                onClick={() => setTooltipClicked(!tooltipClicked)}
-                onMouseEnter={() => setTooltipClicked(true)}
-                onMouseLeave={() => setTooltipClicked(false)}>
+            <Tippy
+              className="text-field-tooltip"
+              trigger={isTablet ? 'mouseenter focus' : 'click'}
+              content={tooltipText}>
+              <figure>
                 <Image src="/assets/Tooltip.svg" width="20" height="20" alt="Hover/Click on me for more information!" />
               </figure>
-            </Tooltip>
+            </Tippy>
           )}
         </div>
         <input
-          className="text-xs h-12 w-full rounded-xl text-neutral-400 px-4 md:px-5 transition-[padding]"
+          className="text-xs h-12 w-full rounded-xl text-neutral-400 transition-[padding,height] px-4 md:px-5 md:h-14"
           placeholder={placeholder}
           type="text"
           name={name}
