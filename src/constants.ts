2dd92adede0f5e5b4a7e5e13b9e17ca3e73863b9
Dexter Bigueta <dexter@course.studio>
2023-08-15 19:50:28 -0700

    Add custom hook to check screensize

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+/* useMediaQuery Screen Sizes */
+export const SCREEN_SM = '(min-width: 37.5em)';
+export const SCREEN_MD = '(min-width: 48em)';
+export const SCREEN_LG = '(min-width: 75em)';
+export const SCREEN_XL = '(min-width: 90em)';
