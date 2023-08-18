5b29baa2f8a85d3ca93f5fec525eddf8a2f253bc
Dexter Bigueta <dexter@course.studio>
2023-08-18 08:44:06 -0700

    use client

--- components/globals/Footer.tsx
+++ components/globals/Footer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 
 const Footer = () => {
@@ -9,7 +11,7 @@ const Footer = () => {
             href="https://www.gasbuddy.com/gaspricemap?lat=48.83148505788142&lng=-122.48382288765794&z=11"
             target="_blank">
             <figure className="scale-transition">
-              <Image src="./assets/gasbuddy.png" width="25" height="25" alt="Click here to see local gas prices!" />
+              <Image src="/assets/gasbuddy.png" width="25" height="25" alt="Click here to see local gas prices!" />
             </figure>
           </a>
           <nav>
@@ -17,14 +19,14 @@ const Footer = () => {
               <li>
                 <a href="https://www.linkedin.com/in/dexter-bigueta" target="_blank">
                   <figure className="scale-transition">
-                    <Image src="./assets/linkedin.svg" width="25" height="25" alt="Click here to view my LinkedIn!" />
+                    <Image src="/assets/linkedin.svg" width="25" height="25" alt="Click here to view my LinkedIn!" />
                   </figure>
                 </a>
               </li>
               <li>
                 <a href="https://github.com/dbigueta" target="_blank">
                   <figure className="scale-transition">
-                    <Image src="./assets/github.svg" width="25" height="25" alt="Click here to view my GitHub!" />
+                    <Image src="/assets/github.svg" width="25" height="25" alt="Click here to view my GitHub!" />
                   </figure>
                 </a>
               </li>
