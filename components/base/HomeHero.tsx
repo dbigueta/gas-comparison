0549271a77c0b4369ed35047c67cc15d81ab0955
Dexter Bigueta <dexter@course.studio>
2023-08-21 23:12:09 -0700

    Reduced hero padding

--- components/base/HomeHero.tsx
+++ components/base/HomeHero.tsx
@@ -111,7 +111,7 @@ const HomeHero = () => {
   };
 
   return (
-    <section className="py-20 md:py-40 transition-[padding]">
+    <section className="py-16 md:py-24 transition-[padding]">
       <div className="wrapper">
         <h1 className="text-xl text-center font-bold mb-12 text-neutral-100">Gas Comparison</h1>
         <form ref={formRef} onSubmit={handleSubmit} className="max-w-[31.25rem] mx-auto md:max-w-[51.5rem]">
